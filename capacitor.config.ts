07c7a78a70a3c653fad207745c7c77591594ace7
kaushal <kaushal2469@gmail.com>
2024-04-13 17:14:22 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'show-spot',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
