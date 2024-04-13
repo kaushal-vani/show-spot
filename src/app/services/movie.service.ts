fb2889e2599a4f2e1d9f607b67309f5861ed3bac
kaushal <kaushal2469@gmail.com>
2024-04-13 17:53:58 +0530

    add function for getTopRatedMovies and getMovieDetails

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = environment.apiKey;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MovieService {
+  private http = inject(HttpClient);
 
-  constructor() { }
+  constructor() {}
+
+  getTopRatedMovies(page = 1) {
+    return this.http.get(
+      `${BASE_URL}/movie/popular?page=${page}&api_key=${API_KEY}`
+    );
+  }
+
+  getMovieDetails(id:string) {
+    return this.http.get(`${BASE_URL}/movie/${id}?api_key=${API_KEY}`)
+  }
 }
