746db6f1594e0565c8657c5938afe34455646343
kaushal <kaushal2469@gmail.com>
2024-04-13 17:38:02 +0530

    add movie API Key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: 'b97403113bbbe00363b8be5e85e4cafd',
 };
 
 /*
