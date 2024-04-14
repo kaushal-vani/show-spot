5d3d4a40cebd0def9dd4e2d608ce923ec5ddd6a7
kaushal <kaushal2469@gmail.com>
2024-04-14 13:23:24 +0530

    create index.ts and export component,pages and services

---
+++ src/app/index.ts
@@ -0,0 +1,10 @@
+//Components
+export * from './latest-movie/latest-movie.component';
+
+//Pages
+export * from './details/details.page';
+export * from './home/home.page';
+
+//Services
+export * from './services/movie.service';
+export * from './services/wishlist.service';
