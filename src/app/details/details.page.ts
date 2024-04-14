4f7760ddffc7cf862cbde67a79eef4819d754af5
kaushal <kaushal2469@gmail.com>
2024-04-14 06:07:05 +0530

    set ID signal in deltails page using angular signals

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -1,20 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  WritableSignal,
+  inject,
+  signal,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
+import { MovieService } from '../services/movie.service';
+import { ApiResult, MovieResult } from '../services/movie.interface';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.page.html',
   styleUrls: ['./details.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+  ],
 })
-export class DetailsPage implements OnInit {
+export class DetailsPage {
+  private movieService = inject(MovieService);
+  public imageBaseUrl = 'https://image.tmdb.org/t/p';
+  public movies: WritableSignal<ApiResult | null> = signal(null);
 
-  constructor() { }
-
-  ngOnInit() {
+  @Input()
+  set id(movieId: string) {
+    this.movieService.getMovieDetails(movieId).subscribe((movie) => {
+      console.log(movie);
+      this.movies.set(movie)
+    });
   }
+
+  constructor() {}
 
 }
