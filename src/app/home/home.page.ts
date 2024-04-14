113c5eea363facbb89a6559d37ffbec80cf5b815
kaushal <kaushal2469@gmail.com>
2024-04-14 06:04:14 +0530

    loadMovies and loadMore functionality

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,95 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, inject, OnInit } from '@angular/core';
+import {
+  InfiniteScrollCustomEvent,
+  IonAlert,
+  IonAvatar,
+  IonContent,
+  IonHeader,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonSkeletonText,
+  IonTitle,
+  IonToolbar,
+  IonBadge,
+  IonInfiniteScroll,
+  IonInfiniteScrollContent,
+} from '@ionic/angular/standalone';
+import { MovieService } from '../services/movie.service';
+import { catchError, finalize } from 'rxjs';
+import { MovieResult } from '../services/movie.interface';
+import { DatePipe } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    IonInfiniteScrollContent,
+    IonInfiniteScroll,
+    IonBadge,
+    DatePipe,
+    IonAlert,
+    IonAvatar,
+    IonContent,
+    IonHeader,
+    IonItem,
+    IonLabel,
+    IonList,
+    IonSkeletonText,
+    IonTitle,
+    IonToolbar,
+    RouterModule,
+  ],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  private currentPage = 1;
+  public dummyArray = new Array(5);
+  public error = null;
+  public imageBaseUrl = 'https://image.tmdb.org/t/p';
+  public movies: MovieResult[] = [];
+  private movieService = inject(MovieService);
+  public isLoading = false;
+
   constructor() {}
+
+  ngOnInit() {
+    this.loadMovies();
+  }
+
+  loadMovies(event?: InfiniteScrollCustomEvent) {
+    this.error = null;
+    if (!event) {
+      this.isLoading = true;
+    }
+    this.movieService
+      .getTopRatedMovies(this.currentPage)
+      .pipe(
+        finalize(() => {
+          this.isLoading = false;
+          if (event) {
+            event.target.complete();
+          }
+        }),
+        catchError((err: any) => {
+          this.error = err.error.status_message;
+          return [];
+        })
+      )
+      .subscribe({
+        next: (res) => {
+          this.movies.push(...res.results);
+          if (event) {
+            event.target.disabled = res.total_pages === this.currentPage;
+          }
+        },
+      });
+  }
+
+  loadMore(event: InfiniteScrollCustomEvent) {
+    this.currentPage++;
+    this.loadMovies(event);
+  }
 }
